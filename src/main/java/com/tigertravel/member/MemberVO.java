4c7501791943414d680cc6b29119daa104e30a6b
lezzy <happylezzy308@gmail.com>
2025-05-29 15:18:30 +0800

    nothing

--- src/main/java/com/tigertravel/member/MemberVO.java
+++ src/main/java/com/tigertravel/member/MemberVO.java
@@ -1,5 +1,19 @@
 package com.tigertravel.member;
 
 public class MemberVO {
-
+	private Integer	ovo;
+	private String member_name;
+	
+	public Integer getOvo() {
+		return ovo;
+	}
+	public void setOvo(Integer ovo) {
+		this.ovo = ovo;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
 }
